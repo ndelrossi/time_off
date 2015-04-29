7ed480e73055cd63920f1cc3dd2b27c24ff28051
Nick DelRossi <nickwtf@gmail.com>
2015-04-29 08:38:03 -0400

    Authenticate user before showing calendar. Make calendar selectable. Add some fake events

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,10 +1,26 @@
 var ready;
 
 ready = function() {
+
   $('#calendar').fullCalendar({
-    weekends: false
-  })
-};
+    selectable: true,
+    weekends: false,
+    events: [
+      {
+      title  : 'Nick',
+      start  : '2015-05-05'
+    },
+    {
+      title  : 'Sandy',
+      start  : '2015-05-14',
+    }
+    ],
+
+    dayClick: function(date, jsEvent, view) {
+      //$(this).css('background-color', 'red');
+    }
+  });
+}
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
